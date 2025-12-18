23731d3c055669ecae4ba49ef80b185ce929043c
dboi <darlinshot@gmail.com>
2025-12-18 19:47:34 +0700

    Renew project

---
+++ src/frontend/web/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button>Click me mommy</button>
+    </div>
+  )
+}
+
+export default Home
--- src/frontend/web/bizflow/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Page() {
-  return (
-    <div style={{ minHeight: "100vh", background: "#0f172a" }}>
-    </div>
-  );
-}
-
