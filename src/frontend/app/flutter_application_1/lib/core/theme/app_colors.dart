847cab838fb0e76f422350c07f0a67ede33e7799
dboi <darlinshot@gmail.com>
2025-12-19 19:44:18 +0700

    Flutter home page, login page, signup page

--- src/frontend/app/flutter_application_1/lib/core/theme/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xFF1E88E5); // xanh dương
-  static const primaryDark = Color(0xFF1565C0);
-  static const background = Color(0xFFF5F7FA);
-}
